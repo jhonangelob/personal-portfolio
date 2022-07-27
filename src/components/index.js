dee1ea4183b8e1fa0aa48e659183d45e989a8bb6
jhonangeloB <bustarde.aj@gmail.com>
2022-07-27 21:31:16 +0800

    Added index file for components import

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Navbar from "./Navbar/Navbar";
+import About from "./About/About";
+import Header from "./Header/Header";
+import Contact from "./Contact/Contact";
+import Experience from "./Experience/Experience";
+import Projects from "./Projects/Projects";
+
+export { Navbar, About, Header, Contact, Experience, Projects };
