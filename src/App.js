84c6aace416b9a0f12fad4f760f870b5d63ed4fc
jhonangeloB <bustarde.aj@gmail.com>
2022-07-28 03:36:58 +0800

    Added all components import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.scss";
-import { Navbar } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Home,
+  Navbar,
+  Projects,
+} from "./components";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Home />
+        <About />
+        <Projects />
+        <Experience />
+        <Contact />
+      </BrowserRouter>
     </div>
   );
 }
