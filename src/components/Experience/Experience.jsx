9afabfcf98de45c73a9534ee63b8a8d0b6ab097a
jhonangeloB <bustarde.aj@gmail.com>
2022-07-28 21:19:07 +0800

    Added Experience component

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import AppWrap from "../../wrapper/AppWrap";
+import { motion } from "framer-motion";
+import "./Experience.scss";
 
 const Experience = () => {
-  return <div>Experience</div>;
+  const [active, setActive] = useState("tech");
+
+  return (
+    <>
+      <div className="cont">
+        <motion.h1
+          exit={{ opacity: 0 }}
+          whileInView={{ opacity: [0, 1] }}
+          transition={{ duration: 0.5 }}
+        >
+          My web development{" "}
+          <span className="tech-btn" onClick={() => setActive("tech")}>
+            technologies
+          </span>{" "}
+          and{" "}
+          <span className="exp-btn" onClick={() => setActive("exp")}>
+            experience
+          </span>
+          .{" "}
+        </motion.h1>
+
+        {active === "tech" ? (
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 1 }}
+            className="tech_cont"
+          >
+            <p>
+              I specialize in Web Development using <span>React</span>,{" "}
+              <span>JavaScript</span>, and <span>Sass</span> for the front-end
+              part. And <span>node.js</span>, firestore <span>Firebase</span>{" "}
+              for the backend part and database.
+            </p>
+            <p>
+              I also have some experience developing website using{" "}
+              <span>PHP</span>, <span>MySQL</span>, and <span>Bootstrap</span>.
+            </p>
+            <p>For the version control,</p>
+            <p>
+              what I use is <span>Git</span> And for the UI/UX Designing. My
+              preffered tool is <span>Figma</span>.
+            </p>
+          </motion.div>
+        ) : (
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 1 }}
+            className="exp-cont"
+          >
+            <p>2022</p>
+            <p>Web developer intern</p>
+            <p>Symph,Inc</p>
+          </motion.div>
+        )}
+      </div>
+    </>
+  );
 };
 
-export default Experience;
+export default AppWrap(Experience, "experience", "app__experience");
