ada31ac26ac3d4d0ab17af2d0a7c9112ba487473
jhonangeloB <bustarde.aj@gmail.com>
2022-07-31 02:08:30 +0800

    Fixed tech and exp animation

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import AppWrap from "../../wrapper/AppWrap";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import "./Experience.scss";
 
 const Experience = () => {
@@ -24,40 +24,46 @@ const Experience = () => {
           </span>
           .{" "}
         </motion.h1>
-
-        {active === "tech" ? (
-          <motion.div
-            whileInView={{ opacity: [0, 1] }}
-            transition={{ duration: 1 }}
-            className="tech_cont"
-          >
-            <p>
-              I specialize in Web Development using <span>React</span>,{" "}
-              <span>JavaScript</span>, and <span>Sass</span> for the front-end
-              part. And <span>node.js</span>, firestore <span>Firebase</span>{" "}
-              for the backend part and database.
-            </p>
-            <p>
-              I also have some experience developing website using{" "}
-              <span>PHP</span>, <span>MySQL</span>, and <span>Bootstrap</span>.
-            </p>
-            <p>For the version control,</p>
-            <p>
-              what I use is <span>Git</span> And for the UI/UX Designing. My
-              preffered tool is <span>Figma</span>.
-            </p>
-          </motion.div>
-        ) : (
-          <motion.div
-            whileInView={{ opacity: [0, 1] }}
-            transition={{ duration: 1 }}
-            className="exp-cont"
-          >
-            <p>2022</p>
-            <p className="exp-title">Web Developer Intern</p>
-            <p>Symph, Inc.</p>
-          </motion.div>
-        )}
+        <AnimatePresence>
+          {active === "tech" ? (
+            <motion.div
+              key={"tech"}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="tech_cont"
+            >
+              <p>
+                I specialize in Web Development using <span>React</span>,{" "}
+                <span>JavaScript</span>, and <span>Sass</span> for the front-end
+                part. And <span>node.js</span>, firestore <span>Firebase</span>{" "}
+                for the backend part and database.
+              </p>
+              <p>
+                I also have some experience developing website using{" "}
+                <span>PHP</span>, <span>MySQL</span>, and <span>Bootstrap</span>
+                .
+              </p>
+              <p>For the version control,</p>
+              <p>
+                what I use is <span>Git</span> And for the UI/UX Designing. My
+                preffered tool is <span>Figma</span>.
+              </p>
+            </motion.div>
+          ) : (
+            <motion.div
+              key={"exp"}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="exp-cont"
+            >
+              <p>2022</p>
+              <p className="exp-title">Web Developer Intern</p>
+              <p>Symph, Inc.</p>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </>
   );
