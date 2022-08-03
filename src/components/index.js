ee98c69b020402e113f9af87dec01e6199bd5427
jhonangeloB <bustarde.aj@gmail.com>
2022-08-03 13:12:11 +0800

    Updates

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Navbar from "./Navbar/Navbar";
-import About from "./About/About";
-import Home from "./Home/Home";
-import Contact from "./Contact/Contact";
-import Experience from "./Experience/Experience";
-import Projects from "./Projects/Projects";
-import Loader from "./Loader/Loader";
-
-export { Navbar, About, Home, Contact, Experience, Projects, Loader };
