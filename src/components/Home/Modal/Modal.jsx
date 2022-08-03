ca6dfa2893f3d1051afff445005fec1410def38e
jhonangeloB <bustarde.aj@gmail.com>
2022-08-03 12:47:59 +0800

    Added backdrop blur and modal animation

--- src/components/Home/Modal/Modal.jsx
+++ src/components/Home/Modal/Modal.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { AiOutlineClose } from "react-icons/ai";
+import { motion } from "framer-motion";
 import "./Modal.scss";
 
 const Modal = ({ ...props }) => {
   if (!props.open) return null;
   console.log(props.file);
   return (
-    <>
+    <div className="app__modal-overlay" onClick={props.closeModal}>
       {props.file?.map((download) => (
-        <div className="app__modal">
+        <motion.div
+          className="app__modal"
+          initial={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
+          whileInView={{ opacity: [0, 1] }}
+          transition={{ duration: 0.3 }}
+        >
           <div className="app__modal-header">
             <button onClick={props.closeModal}>
               <AiOutlineClose />
@@ -31,9 +38,9 @@ const Modal = ({ ...props }) => {
               Get a copy
             </a>
           </div>
-        </div>
+        </motion.div>
       ))}
-    </>
+    </div>
   );
 };
 
