0c35cf4affea36a9b43c90a2b16eb3784ca1c3dc
jhonangeloB <bustarde.aj@gmail.com>
2022-08-15 01:19:04 +0800

    Return Loader animation timeout

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
 
   useEffect(() => {
     setLoading(true);
-    // setTimeout(() => {
-    //   setLoading(false);
-    // }, 2000);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
   }, []);
 
   return (
