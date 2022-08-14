e9170c0c70163edcade6aa341267a9f0c23de0f8
jhonangeloB <bustarde.aj@gmail.com>
2022-08-15 00:43:34 +0800

    Add Particle background animation

---
+++ src/components/Particles/Particles.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Particles.scss";
+const Particles = () => {
+  return (
+    <div class="animation-wrapper">
+      <div class="particle particle-1"></div>
+      <div class="particle particle-2"></div>
+      <div class="particle particle-3"></div>
+      <div class="particle particle-4"></div>
+    </div>
+  );
+};
+
+export default Particles;
