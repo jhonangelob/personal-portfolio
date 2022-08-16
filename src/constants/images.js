852707d0621822d55d6fb892b4880eff3d128fea
jhonangeloB <bustarde.aj@gmail.com>
2022-08-17 00:03:49 +0800

    Add images asset index script

---
+++ src/constants/images.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-anonymous-default-export */
+import html from "../assets/html.svg";
+import css from "../assets/css.svg";
+import reactjs from "../assets/reactjs.svg";
+import sass from "../assets/sass.svg";
+import bootstrap from "../assets/bootstrap.svg";
+import javascript from "../assets/javascript.svg";
+import nodejs from "../assets/nodejs.svg";
+import firebase from "../assets/firebase.svg";
+import git from "../assets/git.svg";
+import npm from "../assets/npm.svg";
+import figma from "../assets/figma.svg";
+import contentful from "../assets/contentful.svg";
+
+export default {
+  html,
+  css,
+  reactjs,
+  sass,
+  javascript,
+  bootstrap,
+  nodejs,
+  firebase,
+  git,
+  npm,
+  figma,
+  contentful,
+};
