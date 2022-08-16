a2d3577f854d1116cc867d6e75f8e9921c26c969
jhonangeloB <bustarde.aj@gmail.com>
2022-08-17 00:04:08 +0800

    New experience and tech stack ui

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,71 +1,62 @@
-import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
+import React from "react";
+import images from "../../constants/images";
 import AppWrap from "../../wrapper/AppWrap";
 import "./Experience.scss";
 
 const Experience = () => {
-  const [active, setActive] = useState("tech");
-
   return (
     <>
-      <div className="cont">
-        <motion.h1
+      <motion.h1 whileInView={{ opacity: [0, 1] }} transition={{ duration: 1 }}>
+        My Web Development <span>Technologies</span> and <span>Experience</span>
+        .
+      </motion.h1>
+      <div className="app__experience-cont">
+        <motion.div
           whileInView={{ opacity: [0, 1] }}
           transition={{ duration: 1 }}
+          className="tech_cont"
         >
-          My Web Development{" "}
-          <span
-            className={`tech-btn ${active === "tech" ? "active" : ""}`}
-            onClick={() => setActive("tech")}
-          >
-            Technologies
-          </span>{" "}
-          and{" "}
-          <span
-            className={`exp-btn ${active === "exp" ? "active" : ""}`}
-            onClick={() => setActive("exp")}
-          >
-            Experience
-          </span>
-          .{" "}
-        </motion.h1>
-        <AnimatePresence>
-          {active === "tech" ? (
-            <motion.div
-              key={"tech"}
-              whileInView={{ opacity: [0, 1] }}
-              transition={{ duration: 1 }}
-              className="tech_cont"
-            >
-              <p>
-                I specialize in Front-End Web Development with{" "}
-                <span>React</span>, <span>JavaScript</span>,{" "}
-                <span>Bootstrap</span>, and <span>Sass</span>. As well as{" "}
-                <span>node.js</span> and <span>Firebase</span> for the backend
-                and database.
-              </p>
-              <p>Among other technologies that I use are:</p>
-              <p>
-                <span>Git</span> for version control
-              </p>
-              <p>
-                <span>Figma</span> for UI/UX Design
-              </p>
-            </motion.div>
-          ) : (
-            <motion.div
-              key={"exp"}
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: [0, 1] }}
-              transition={{ duration: 0.5 }}
-              className="exp-cont"
-            >
-              <p>2022</p>
-              <p className="exp-title">Web Developer Intern</p>
-              <p>Symph, Inc.</p>
-            </motion.div>
-          )}
-        </AnimatePresence>
+          <h4>Technologies</h4>
+          <p>
+            <em>These are the technologies I use for front-end development</em>{" "}
+          </p>
+          <div className="app__experience-card">
+            <img src={images.html} alt="html" />
+            <img src={images.css} alt="css" />
+            <img src={images.reactjs} alt="react" />
+            <img src={images.sass} alt="sass" />
+            <img src={images.javascript} alt="javascript" />
+            <img src={images.bootstrap} alt="bootstrap" />
+          </div>
+          <p>
+            <em>These are the technologies I use for back-end development</em>{" "}
+          </p>
+          <div className="app__experience-card">
+            <img src={images.nodejs} alt="nodejs" />
+            <img src={images.firebase} alt="firebase" />
+          </div>
+          <p>
+            <em>Others...</em>{" "}
+          </p>
+          <div className="app__experience-card">
+            <img src={images.git} alt="git" />
+            <img src={images.npm} alt="npm" />
+            <img src={images.figma} alt="figma" />
+            <img src={images.contentful} alt="contentful" />
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: [0, 1] }}
+          transition={{ duration: 0.5 }}
+          className="exp_cont"
+        >
+          <h4>Experience</h4>
+          <p>2022</p>
+          <p className="exp-title">Web Developer Intern</p>
+          <p>Symph, Inc.</p>
+        </motion.div>
       </div>
     </>
   );
